55814249c84aac23c54e3bca3063403e48eb977a
tolotrasam <tolotrasam@gmail.com>
2017-05-01 17:04:16 +0400

    Initial commit

--- wp-content/upgrade/wordpress-4.7.4-WOnH1M/wordpress/wp-includes/default-constants.php
+++ wp-content/upgrade/wordpress-4.7.4-WOnH1M/wordpress/wp-includes/default-constants.php
@@ -288,7 +288,7 @@ function wp_ssl_constants() {
 		if ( 'https' === parse_url( get_option( 'siteurl' ), PHP_URL_SCHEME ) ) {
 			define( 'FORCE_SSL_ADMIN', true );
 		} else {
-			define( 'FORCE_SSL_ADMIN', false );
+			define( 'FORCE_SSL_ADMIN', true );
 		}
 	}
 	force_ssl_admin( FORCE_SSL_ADMIN );
